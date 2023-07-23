66ec6074516b25f91b90844f8a1d939afb7525bc
PR1YAM9 <priyam9maini@gmail.com>
2023-07-23 22:42:12 +0530

    font corrected

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -18,18 +18,18 @@ const Hero = ({ menuOpen }) => {
           <p className='hero-sub1 p1'>
             Hi ! I'm<br />
             <span style={{ fontWeight: 'bold' }}>Pratik Nikam.</span><br />
-            <span style={{ color: '#0CA4E6' }}>from IIT Hyderabad</span>
+            <span className='fontg' style={{ color: '#0CA4E6' }}>from IIT Hyderabad</span>
           </p>
         ) : (
           !isMobile && !menuOpen && ( // Hide p1 on larger screens when menuOpen is true
             <p className='hero-sub1 p1'>
-              Hi ! my name is <span style={{ fontWeight: "bold" }}>Pratik <br /> Nikam</span>. I am an Electrical Engineering student <br /> <span style={{ color: '#0CA4E6' }}>@IIT Hyderabad</span>
+              Hi ! my name is <span style={{ fontWeight: "bold" }}>Pratik <br /> Nikam</span>. I am an Electrical Engineering student <br /> <span  className='fontg'  style={{ color: '#0CA4E6' }}>@IIT Hyderabad</span>
             </p>
           )
         )}
         {!menuOpen && ( // Hide p2 when menuOpen is true
           <p className='hero-sub1 p2'>
-            Previously <span style={{ color: '#0CA4E6' }}>Interned <br />@eYantra</span> , IIT Bombay
+            Previously <span  className='fontg' style={{ color: '#0CA4E6' }}>Interned <br />@eYantra</span> , IIT Bombay
           </p>
           
         )}
